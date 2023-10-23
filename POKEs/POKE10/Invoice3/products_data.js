6d7a718795e683c1c7dcd912bd51e40476dabc57
hanibae <haeunb@hawaii.edu>
2023-10-23 21:11:21 +0900

    Message

---
+++ POKEs/POKE10/Invoice3/products_data.js
@@ -0,0 +1,32 @@
+let quantity = [1,3,2,1,1];
+//let quantity = [2, "cat", 1.5, -1.5, 0];
+
+let itemData = [
+    { 
+        brand: "HTC",
+        price: 40.00,
+        quantityIndex: 0
+    },
+    {
+        brand: "Apple",
+        price: 75.00,
+        quantityIndex: 1
+    },
+    {
+        brand: "Nokia",
+        price: 35.00,
+        quantityIndex: 2
+    },
+    {
+        brand: "Samsung",
+        price: 45.00,
+        quantityIndex: 3
+    },
+    {
+        brand: "Blackberry",
+        price: 10.00,
+        quantityIndex: 4
+    }
+];
+
+export{itemData, quantity};
